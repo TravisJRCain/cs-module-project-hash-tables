ff5c2da0d5ff86cd2aa7cbaabc5a035960617013
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-08-05 15:56:07 -0700

    Module 3 complete

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,26 @@
 # Your code here
-
+cache = {}
 
 def expensive_seq(x, y, z):
     # Your code here
-
+    # start with if x is less than or equal to zero
+    if x <= 0:
+        # if the element is in our cache already, return
+        if (x, y, z) in cache:
+            return cache[(x, y, z)]
+        # set current slot
+        cache[(x, y, z)] = y + z
+        # return current slot
+        return cache[(x, y, z)]
+    # if x is greater than zero
+    if x > 0:
+        # element in cache already
+        if (x, y, z) in cache:
+            return cache[(x, y, z)]
+        # set current slot
+        cache[(x, y, z)] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+        # return current slot
+        return cache[(x, y, z)]
 
 
 if __name__ == "__main__":

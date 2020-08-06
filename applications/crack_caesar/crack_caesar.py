a0f192da33e87934a35c9ff60deac8279d571e37
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-08-06 16:11:42 -0700

    Module 4 done

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,50 @@
 
 # Your code here
 
+from collections import defaultdict
+
+# common letters
+freq_list = [
+    "E",
+    "T",
+    "A",
+    "O",
+    "H",
+    "N",
+    "R",
+    "I",
+    "S",
+    "D",
+    "L",
+    "W",
+    "U",
+    "G",
+    "F",
+    "B",
+    "M",
+    "Y",
+    "C",
+    "P",
+    "K",
+    "V",
+    "Q",
+    "J",
+    "X",
+    "Z",
+]
+
+# open file
+with open("ciphertext.txt") as file:
+    ciphertext = file.read()
+
+counts = defaultdict(int)
+
+for c in ciphertext:
+    if c.isupper():
+        counts[c] += 1
+
+cipher_freq = [item[0] for item in sorted(counts.items(), key=lambda x: x[1], reverse=True)]
+
+lookup = {cipher: plain for cipher, plain in zip(cipher_freq, freq_list)}
+
+print(ciphertext.translate(str.maketrans(lookup)))
